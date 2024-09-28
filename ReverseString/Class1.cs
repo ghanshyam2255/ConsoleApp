33a7330288813383218e6d59958990abb2ee788f
Ghanshyam_Patel <patelsarkar2255@gmail.com>
2024-09-28 16:48:28 +0530

    First commit

---
+++ ReverseString/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System;
+
+public class ReversePattern
+{
+    public void Name()
+    {
+        string str = "GhANSHYAM";
+        string reversedString = "GHANSHYAM";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reversedString += str[i];
+        }
+
+        Console.WriteLine("Reversed String: " + reversedString);
+    }
+}
